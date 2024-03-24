1ec4d4ff73d6471e038ccc32f2d696eb02c71f1e
Natan <zerogamerptbr@gmail.com>
2024-03-24 17:42:34 -0300

    :sparkles: feat: atividade 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 //import { Pai } from "./components/atividade01/questao01/Pai";
 //import * as meuPc from "./components/atividade01/questao02/MeuPc.jsx";
-import { Arena } from "./components/atividade01/questao03/Arena.jsx";
-import World from "./components/atividade01/questao03/World.jsx";
+//import { Arena } from "./components/atividade01/questao03/Arena.jsx";
+//import World from "./components/atividade01/questao03/World.jsx";
+//import FunctionContextA from "./components/atividade02/questao01/FunctionContextA.jsx";
+
+import PokemonComponent from "./components/atividade02/questao03/PokemonComponent";
+
+//import PokemonAvo from "./components/atividade02/questao02/PokemonAvo.jsx";
 function App() {
   /*  
 RETORNO DA QUESTAO 1
@@ -18,16 +23,28 @@ return (
   //     <meuPc.placaMae precoPlacaMae={30} nomePlacaMae="Asus" />
   //   </div>
   // );
-  return (
-    <div>
-      <World>
-        <Arena></Arena>
-        <Arena></Arena>
+  // return (
+  //   <div>
+  //     <World>
+  //       <Arena></Arena>
+  //       <Arena></Arena>
 
-        <Arena></Arena>
-      </World>
-    </div>
-  );
+  //       <Arena></Arena>
+  //     </World>
+  //   </div>
+  // );
+
+  //QUESTAO 1 ATIVIDADE 2
+
+  // return (
+  //   <div>
+  //     <FunctionContextA />
+  //   </div>
+  // );
+  //QUESTAO 2
+  // return <PokemonAvo />;
+
+  return <PokemonComponent />;
 }
 
 export default App;
